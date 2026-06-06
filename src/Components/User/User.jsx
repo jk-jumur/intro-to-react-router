60d0d4441f27dca1155ed26fd2ec57936f29b78e
jk-jumur <developer.jumur@gmail.com>
2026-06-07 02:26:18 +0600

    load data in the component without router and not found 404 route

--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,7 +1,12 @@
+import { Suspense, useState } from "react";
 import { Link } from "react-router";
+import UserDetails2 from "../UserDetails2/UserDetails2";
 
 const User = ({user}) => {
+     const [showInfo, setShowInfo] = useState(false);
      const {id, name, email, username, phone, website} = user;
+
+        const userPromise = fetch(`https://jsonplaceholder.typicode.com/users/${id}`).then(res => res.json());
 
      const userStyle = {
          border: '2px solid greenYellow',
@@ -17,6 +22,14 @@ const User = ({user}) => {
                  <p>username: {username}</p>
                  <p>website: {website}</p>
                  <Link to={`/users/${id}`}>Show Details Here</Link>
+                 <button onClick={() => setShowInfo(!showInfo)}>{showInfo ? 'Hide' :'Show'} info</button>
+
+                 {
+                      showInfo && <Suspense fallback={<span>Loading.....</span>}>
+
+                        <UserDetails2 userPromise={userPromise}></UserDetails2>
+                      </Suspense>
+                 }
         </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -83,6 +83,11 @@ const router = createBrowserRouter([
         path: 'posts/:postId',
         loader: ({params}) => fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`),
         Component: PostDetail
+       },
+
+       {
+        path: '*',
+        element: <h3>Not Found: 404 Status</h3>
        }
     ]
   }
